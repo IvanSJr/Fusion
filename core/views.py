b2b7a1fa5dbecb6ecb6d24c3a1ebcb9143124796
IvanSJr <ivanjrjesus01@gmail.com>
2021-03-22 12:28:35 -0300

    Adicionando metodos de formulário form_valid e form_invalid. Importando o reverse_lazy, messages. Utilizando messages.success e messages.error.

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Servico, Funcionario, Recurso
+from .forms import ContatoForm
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
 
     # Context Manager
     def get_context_data(self, **kwargs):
@@ -15,3 +20,12 @@ class IndexView(TemplateView):
         context["recursos1"] = recursos[:c//2]  # lado esquerdo do layout
         context["recursos2"] = recursos[c//2:c]  # lado direito do layout
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Ocorreu um erro ao enviar o email!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
