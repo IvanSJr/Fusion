5a4926e63492f08c6285743286fb5c13ef94ecf9
IvanSJr <ivanjrjesus01@gmail.com>
2021-03-22 11:40:39 -0300

    Adicionando model ao painel administrativo do django.

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Cargo, Servico, Funcionario
+from .models import Cargo, Servico, Funcionario, Recurso
 
 @admin.register(Cargo)
 class CargoAdmin(admin.ModelAdmin):
@@ -15,3 +15,8 @@ class ServicoAdmin(admin.ModelAdmin):
 @admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'cargo', 'ativo', 'modificado')
+
+
+@admin.register(Recurso)
+class RecursoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'icone', 'descricao')
