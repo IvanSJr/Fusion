139515484eedf6aca7b3f83246a98c91904e4edc
IvanSJr <ivanjrjesus01@gmail.com>
2021-03-22 11:29:36 -0300

    Gerenciando context manager. Buscando dados do banco de dados. Iterando dados nos templates.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
+from .models import Servico, Funcionario
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    # Context Manager
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
