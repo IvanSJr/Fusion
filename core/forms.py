6eb9f8c3e5583ff464da3c53edd15db2d43c7ca7
IvanSJr <ivanjrjesus01@gmail.com>
2021-03-22 12:23:06 -0300

    Criando model de contato utilizando o EmailMessage.

---
+++ core/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='Email', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=100)
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        conteudo = f'Nome: {nome}\nEmail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}'
+
+        mail = EmailMessage(
+            subject=assunto,
+            body=conteudo,
+            from_email='ivanjrjesus01@gmail.com',
+            to=['ivan.junior2706@gmail.com', 'ivanoloko100@gmail.com', 'contato@fusion.com.br'],
+            headers={'Reaply-to': email}
+        )
+        mail.send()
